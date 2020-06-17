b45c714b4bfce4320ead16d4075270b7d3e188fa
김석환 <tokany@naver.com>
2020-06-17 14:56:55 +0900

    final...?

--- posts/forms.py
+++ posts/forms.py
@@ -1,10 +1,25 @@
 from django import forms
 from .models import Post, Comment
 
+SCORE_CHOICES= [
+    ( 1, '1'),
+    ( 2, '2'),
+    ( 3, '3'),
+    ( 4, '4'),
+    ( 5, '5'),
+]
+
 class PostForm(forms.ModelForm):
+    score = forms.IntegerField(
+    label='Rank this movie :)',
+    widget=forms.RadioSelect(choices=SCORE_CHOICES, attrs={
+        'class': 'form-check-inline'}
+        )
+    )
+
     class Meta:
         model = Post
-        fields = ['score', 'title', 'content']
+        fields = ['title', 'score', 'content']
 
 class CommentForm(forms.ModelForm):
     class Meta:
